964be2bf9306596e9757495136cd733d4791ce2c
Ubuntu <ubuntu@ip-172-31-86-193.ec2.internal>
2020-01-15 18:19:35 +0000

    map styles

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,124 @@
-const map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 7,
   center: {lat: 52.632469, lng: -1.689423},
   styles: mapStyle
-});
+  });
 
-map.data.loadGeoJson('stores.json');
+  map.data.loadGeoJson('stores.json');
+  
+  map.data.setStyle(feature => {
+    return {
+      icon: {
+        url: `img/icon_${feature.getProperty('category')}.png`,
+        scaledSize: new google.maps.Size(64, 64)
+      }
+    };
+  });
+  
+  map.data.addListener('click', event => {
+    let category = event.feature.getProperty('category');
+    let name = event.feature.getProperty('name');
+    let description = event.feature.getProperty('description');
+    let hours = event.feature.getProperty('hours');
+    let phone = event.feature.getProperty('phone');
+    let position = event.feature.getGeometry().get();
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    infoWindow.open(map);
+  });
+}
 
-map.data.setStyle(feature => {
-  return {
-    icon: {
-      url: `img/icon_${feature.getProperty('category')}.png`,
-      scaledSize: new google.maps.Size(64, 64)
-    }
-  };
-});
-
-map.data.addListener('click', event => {
-  let category = event.feature.getProperty('category');
-  let name = event.feature.getProperty('name');
-  let description = event.feature.getProperty('description');
-  let hours = event.feature.getProperty('hours');
-  let phone = event.feature.getProperty('phone');
-  let position = event.feature.getGeometry().get();
-  let content = `
-    <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
-    <div style="margin-left:220px; margin-bottom:20px;">
-      <h2>${name}</h2><p>${description}</p>
-      <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
-      <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat(52.632469)},${position.lng(-1.689423)}&key=${AIzaSyAqthnqqZil9T4Tpz-2y9S13JjASnjjHPg}"></p>
-    </div>
-  `;
-  infoWindow.setContent(content);
-  infoWindow.setPosition(position);
-  infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
-  infoWindow.open(map);
-});
+const mapStyle = [
+  {
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 33
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f2e5d4"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5dac6"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5c6c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e4d7c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fbfaf7"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#acbcc9"
+      }
+    ]
+  }
+];
