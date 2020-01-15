90d190955bbdc8bf6932b97fe022324afd1029ae
Ubuntu <ubuntu@ip-172-31-86-193.ec2.internal>
2020-01-15 18:50:10 +0000

    stores.json

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,12 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 7,
-  center: {lat: 52.632469, lng: -1.689423},
-  styles: mapStyle
+    zoom: 7,
+    center: { lat: 52.632469, lng: -1.689423 },
+    styles: mapStyle
   });
 
   map.data.loadGeoJson('stores.json');
-  
+
   map.data.setStyle(feature => {
     return {
       icon: {
@@ -15,7 +15,7 @@ function initMap() {
       }
     };
   });
-  
+
   map.data.addListener('click', event => {
     let category = event.feature.getProperty('category');
     let name = event.feature.getProperty('name');
@@ -23,12 +23,23 @@ function initMap() {
     let hours = event.feature.getProperty('hours');
     let phone = event.feature.getProperty('phone');
     let position = event.feature.getGeometry().get();
+    let content = `
+    <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
+    <div style="margin-left:220px; margin-bottom:20px;">
+      <h2>${name}</h2><p>${description}</p>
+      <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+      <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}"></p>
+    </div>
+  `;
     infoWindow.setContent(content);
     infoWindow.setPosition(position);
-    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    infoWindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
     infoWindow.open(map);
   });
 }
+
+const apiKey = 'AIzaSyAqthnqqZil9T4Tpz-2y9S13JjASnjjHPg';
+const infoWindow = new google.maps.InfoWindow();
 
 const mapStyle = [
   {
@@ -122,3 +133,159 @@ const mapStyle = [
     ]
   }
 ];
+
+ const card = document.createElement('div');
+ const titleBar = document.createElement('div');
+ const title = document.createElement('div');
+ const container = document.createElement('div');
+ const input = document.createElement('input');
+ const options = {
+   types: ['address'],
+   componentRestrictions: { country: 'gb' },
+ };
+
+ card.setAttribute('id', 'pac-card');
+ title.setAttribute('id', 'title');
+ title.textContent = 'Find the nearest store';
+ titleBar.appendChild(title);
+ container.setAttribute('id', 'pac-container');
+ input.setAttribute('id', 'pac-input');
+ input.setAttribute('type', 'text');
+ input.setAttribute('placeholder', 'Enter an address');
+ container.appendChild(input);
+ card.appendChild(titleBar);
+ card.appendChild(container);
+ map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+ const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+ autocomplete.setFields(
+   ['address_components', 'geometry', 'name']);
+
+ const originMarker = new google.maps.Marker({ map: map });
+ originMarker.setVisible(false);
+ let originLocation = map.getCenter();
+
+ autocomplete.addListener('place_changed', async() => {
+   originMarker.setVisible(false);
+   originLocation = map.getCenter();
+   const place = autocomplete.getPlace();
+
+   if (!place.geometry) {
+     window.alert('No address available for input: \'' + place.name + '\'');
+     return;
+   }
+
+   originLocation = place.geometry.location;
+   map.setCenter(originLocation);
+   map.setZoom(9);
+   console.log(place);
+
+   originMarker.setPosition(originLocation);
+   originMarker.setVisible(true);
+
+   const rankedStores = await calculateDistances(map.data, originLocation);
+   showStoresList(map.data, rankedStores);
+
+   return;
+ });
+ }
+ 
+async function calculateDistances(data, origin) {
+  const stores = [];
+  const destinations = [];
+
+  // Build parallel arrays for the store IDs and destinations
+  data.forEach((store) => {
+    const storeNum = store.getProperty('storeid');
+    const storeLoc = store.getGeometry().get();
+
+    stores.push(storeNum);
+    destinations.push(storeLoc);
+  });
+
+  // Retrieve the distances of each store from the origin
+  // The returned list will be in the same order as the destinations list
+  const service = new google.maps.DistanceMatrixService();
+  const getDistanceMatrix =
+    (service, parameters) => new Promise((resolve, reject) => {
+      service.getDistanceMatrix(parameters, (response, status) => {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          reject(response);
+        } else {
+          const distances = [];
+          const results = response.rows[0].elements;
+          for (let j = 0; j < results.length; j++) {
+            const element = results[j];
+            const distanceText = element.distance.text;
+            const distanceVal = element.distance.value;
+            const distanceObject = {
+              storeid: stores[j],
+              distanceText: distanceText,
+              distanceVal: distanceVal,
+            };
+            distances.push(distanceObject);
+          }
+
+          resolve(distances);
+        }
+      });
+    });
+
+  const distancesList = await getDistanceMatrix(service, {
+    origins: [origin],
+    destinations: destinations,
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.METRIC,
+  });
+
+  distancesList.sort((first, second) => {
+    return first.distanceVal - second.distanceVal;
+  });
+
+  return distancesList;
+  }
+  
+  function showStoresList(data, stores) {
+    if (stores.length == 0) {
+      console.log('empty stores');
+      return;
+    }
+
+    let panel = document.createElement('div');
+    if (document.getElementById('panel')) {
+      panel = document.getElementById('panel');
+      // If panel is already open, close it
+      if (panel.classList.contains('open')) {
+        panel.classList.remove('open');
+      }
+    }
+    else {
+      panel.setAttribute('id', 'panel');
+      const body = document.body;
+      body.insertBefore(panel, body.childNodes[0]);
+    }
+
+
+    // Clear the previous details
+    while (panel.lastChild) {
+      panel.removeChild(panel.lastChild);
+    }
+
+    stores.forEach((store) => {
+      const name = document.createElement('p');
+      name.classList.add('place');
+      const currentStore = data.getFeatureById(store.storeid);
+      name.textContent = currentStore.getProperty('name');
+      panel.appendChild(name);
+      const distanceText = document.createElement('p');
+      distanceText.classList.add('distanceText');
+      distanceText.textContent = store.distanceText;
+      panel.appendChild(distanceText);
+    });
+
+    panel.classList.add('open');
+
+    return;
+  }
+}
