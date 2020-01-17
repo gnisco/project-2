8b39471d63c481f838e360b390445d7a58dd93cb
Ubuntu <ubuntu@ip-172-31-86-193.ec2.internal>
2020-01-17 16:47:33 +0000

    list.js upate

--- assets/js/list.js
+++ assets/js/list.js
@@ -81,22 +81,127 @@ function filterFunction() {
 
 
 
-
-var car_list = new List('car_list', range, price);
-car_list.sort("price", {
-  order: "asc"
-})
-
-$('.filter').on('click',function(){
-  var $q = $(this).attr('data-filter');
-  if($(this).hasClass('active')){
-    albums.filter();
-    $('.filter').removeClass('active');
-  } else {
-    albums.filter(function(item) {
-      return (item.values().date == $q);
+ 
+$.fn.listfilter = function(options) {
+  options = $.extend(true, $.fn.listfilter.defaultOptions, options);
+ 
+  return this.each(function() {
+    if($(this).hasClass('car_list-processed')) {
+      refreshFilters(this);
+      return;
+    }
+    var table = $(this);
+    var start = new Date();
+ 
+    $('th:visible', table).each(function(index) {
+      if($(this).hasClass('skip-filter')) return;
+      var selectbox = $('<select class="form-control">');
+      var values = [];
+      var opts = [];
+      selectbox.append('<option value="--all--">' + $(this).text() + '</option>');
+ 
+      var col = $('tr:not(.skip-filter) td:nth-child(' + (index + 1) + ')', table).each(function() {
+        var cellVal = options.valueCallback.apply(this);
+        if(cellVal.length == 0) {
+          cellVal = '--empty--';
+        }
+        $(this).attr('ddtf-value', cellVal);
+ 
+        if($.inArray(cellVal, values) === -1) {
+          var cellText = options.textCallback.apply(this);
+          if(cellText.length == 0) {cellText = options.emptyText;}
+          values.push(cellVal);
+          opts.push({val:cellVal, text:cellText});
+        }
+      });
+      if(opts.length < options.minOptions){
+        return;
+      }
+      if(options.sortOpt) {
+        opts.sort(options.sortOptCallback);
+      }
+      $.each(opts, function() {
+        $(selectbox).append('<option value="' + this.val + '">' + this.text + '</option>')
+      });
+ 
+      $(this).wrapInner('<div style="display:none">');
+      $(this).append(selectbox);
+ 
+      selectbox.bind('change', {column:col}, function(event) {
+        var changeStart = new Date();
+        var value = $(this).val();
+ 
+        event.data.column.each(function() {
+          if($(this).attr('ddtf-value') === value || value == '--all--') {
+            $(this).removeClass('car_list-filtered');
+          }
+          else {
+            $(this).addClass('car_list-filtered');
+          }
+        });
+        var changeStop = new Date();
+        if(options.debug) {
+          console.log('Search: ' + (changeStop.getTime() - changeStart.getTime()) + 'ms');
+        }
+        refreshFilters(table);
+ 
+      });
+      table.addClass('ddtf-processed');
+      if($.isFunction(options.afterBuild)) {
+        options.afterBuild.apply(table);
+      }
     });
-    $('.filter').removeClass('active');
-    $(this).addClass('active');
-  }
-});
+ 
+    function refreshFilters(table) {
+      var refreshStart = new Date();
+      $('tr', table).each(function() {
+        var row = $(this);
+        if($('td.ddtf-filtered', row).length > 0) {
+          options.transition.hide.apply(row, options.transition.options);
+        }
+        else {
+          options.transition.show.apply(row, options.transition.options);
+        }
+      });
+ 
+      if($.isFunction(options.afterFilter)) {
+        options.afterFilter.apply(table);
+      }
+ 
+      if(options.debug) {
+        var refreshEnd = new Date();
+        console.log('Refresh: ' + (refreshEnd.getTime() - refreshStart.getTime()) + 'ms');
+      }
+    }
+ 
+    if(options.debug) {
+      var stop = new Date();
+      console.log('Build: ' + (stop.getTime() - start.getTime()) + 'ms');
+    }
+  });
+};
+ 
+$.fn.ddTableFilter.defaultOptions = {
+  valueCallback:function() {
+    return encodeURIComponent($.trim($(this).text()));
+  },
+  textCallback:function() {
+    return $.trim($(this).text());
+  },
+  sortOptCallback: function(a, b) {
+    return a.text.toLowerCase() > b.text.toLowerCase();
+  },
+  afterFilter: null,
+  afterBuild: null,
+  transition: {
+    hide:$.fn.hide,
+    show:$.fn.show,
+    options: []
+  },
+  emptyText:'--Empty--',
+  sortOpt:true,
+  debug:false,
+  minOptions:2
+}
+ 
+})(jQuery);
